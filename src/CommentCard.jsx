3de86134315197fb2b41d0d8bafa46866c47b4ad
VenturingForth <opilapro@gmail.com>
2024-02-07 12:25:19 +0000

    Comment deletion added

--- src/CommentCard.jsx
+++ src/CommentCard.jsx
@@ -1,12 +1,45 @@
-import { Card, CardHeader } from "react-bootstrap";
+import { useContext, useState } from "react";
+import { Card, Button } from "react-bootstrap";
+import UserContext from "./contexts/UserContext";
+import { deleteComment } from "./api/api";
+
 
 export default function CommentsCard({comment}){
+    const { loggedInUser } = useContext(UserContext);
+    const [ isDeleting, setIsDeleting ] = useState(false);
+    const [ isError, setIsError ] = useState(false);
+    
+    function handleDeleteClick(event){
+        setIsDeleting(true);
+        deleteComment(comment.comment_id)
+        .then((data) => {
+            setIsDeleting(false);
+            if(isError){
+                setIsError(false);
+            }
+        }).catch((err) => {
+            setIsError(true);
+            setIsDeleting(false);
+            console.log(err);
+        })
+    }
+
     return (
         <Card style={{width: '36rem'}}>
             <Card.Body>
                 <Card.Header>{`${comment.author} @ ${comment.created_at}`}</Card.Header>
                 <Card.Text>{`${comment.body}`}</Card.Text>
                 <Card.Footer>{`Votes: ${comment.votes}`}</Card.Footer>
+                {(loggedInUser.username === comment.author) ?
+                <Button 
+                    variant="danger" 
+                    type="button" 
+                    disabled={isDeleting}
+                    onClick={() => {handleDeleteClick(event)}}>
+                        Delete
+                    </Button> :
+                null}
+                <Card.Text>{isError ? "Your comment could not be deleted, please try again" : null}</Card.Text>
             </Card.Body>
         </Card>
     )
